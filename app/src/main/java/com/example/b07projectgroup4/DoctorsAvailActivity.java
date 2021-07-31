96217f2d5260c3f9ae7af45bb59944e25a7da537
vishal-sahoo <vishaloakridge@gmail.com>
2021-07-31 23:09:02 +0530

    Created the required activities

---
+++ app/src/main/java/com/example/b07projectgroup4/DoctorsAvailActivity.java
@@ -0,0 +1,14 @@
+package com.example.b07projectgroup4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DoctorsAvailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctors_avail);
+    }
+}
---
+++ app/src/main/java/com/example/b07projectgroup4/ListDoctorsActivity.java
@@ -0,0 +1,14 @@
+package com.example.b07projectgroup4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListDoctorsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_doctors);
+    }
+}
