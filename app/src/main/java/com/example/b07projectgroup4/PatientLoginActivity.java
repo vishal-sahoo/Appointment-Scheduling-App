bfd8b70e8a6179b7dc2fe78a164d5b2ba27de52d
vishal-sahoo <vishaloakridge@gmail.com>
2021-07-31 11:24:46 +0530

    Successfully set up Firebase Realtime Database

--- app/src/main/java/com/example/b07projectgroup4/PatientLoginActivity.java
+++ app/src/main/java/com/example/b07projectgroup4/PatientLoginActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class PatientLoginActivity extends AppCompatActivity {
 
     @Override
