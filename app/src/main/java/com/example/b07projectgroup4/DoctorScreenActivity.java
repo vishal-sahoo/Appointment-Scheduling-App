a96e05dd6532c340198c303edca5d80577887a53
Hui Z <selina.zhi@mail.utoronto.ca>
2021-08-08 01:07:05 -0400

    Fixed doctor screen crash issue

--- app/src/main/java/com/example/b07projectgroup4/DoctorScreenActivity.java
+++ app/src/main/java/com/example/b07projectgroup4/DoctorScreenActivity.java
@@ -1,71 +1,70 @@
 package com.example.b07projectgroup4;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 import android.content.Intent;
+import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
 
-import androidx.annotation.NonNull;
-
+//import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-//import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
-import java.util.ArrayList;
-import java.util.List;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.AdapterView;
+
+//import com.google.firebase.database.Query;
+//import java.lang.reflect.Array;
+import java.util.*;
 
 public class DoctorScreenActivity extends AppCompatActivity {
-
-
+    Doctor passed_doctor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor_screen);
 
-
-
-
         Intent intent = getIntent();
-        Doctor doctor = (Doctor) intent.getSerializableExtra("doctor");
-
-
+        Doctor doctor = (Doctor)intent.getSerializableExtra("doctor");
+        passed_doctor = doctor;
 
         String name = doctor.getName();
         TextView doctorName = findViewById(R.id.doctorPointer);
-        doctorName.append(name);
-        ListView displayAppointments;
-        ArrayAdapter<Appointment> arrayAdapter;
+        doctorName.append(" " + name);
 
-        displayAppointments = findViewById(R.id.display_appointment);
+        ListView display_appointments;
 
-        List<Appointment> appointments = new ArrayList<>();
+        List<String> appointments = new ArrayList<>();
+
+        ArrayAdapter<String> arrayAdapter;
+
+        display_appointments = (ListView)findViewById(R.id.display_appointment);
+
 
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference ref = database.getReference("doctors");
-
-        DatabaseReference ref2 = ref.child(doctor.getUsername()).child("upcoming_appointments");
-
-        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, appointments);
-        displayAppointments.setAdapter(arrayAdapter);
+        DatabaseReference ref2 = ref.child(doctor.getUsername()).child("upcoming_appointments").getRef();
+        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, appointments);
+        display_appointments.setAdapter(arrayAdapter);
 
         ref2.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 appointments.clear();
                 for (DataSnapshot child : dataSnapshot.getChildren()){
-                    Appointment newAppointment = child.getValue(Appointment.class);
-                    appointments.add(newAppointment);
+                    Appointment new_appointment = child.getValue(Appointment.class);
+                    if(new_appointment == null){
+                        return;
+                    }
+                    appointments.add(new_appointment.displayForPatient());
                 }
                 if(arrayAdapter != null){
                     arrayAdapter.notifyDataSetChanged();
@@ -78,27 +77,25 @@ public class DoctorScreenActivity extends AppCompatActivity {
             }
         });
 
-
-
-            displayAppointments.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        display_appointments.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapter, View view, int position, long arg) {
                 Intent intent1 = new Intent(DoctorScreenActivity.this, PatientDetailsActivity.class);
-                Appointment selectedAppointment = (Appointment)appointments.get(position);
-                intent.putExtra("appointment", selectedAppointment);
+                //Appointment appointment = (Appointment) appointments.get(position);
+                //intent.putExtra("appointment", selectedAppointment);
                 startActivity(intent1);
             }
 
+
+
         });
+
 
     }
 
-
-
-        public void view_schedule(View view){
-            Intent intent2 = new Intent(getApplicationContext(), DoctorScheduleActivity.class);
-            intent2.putExtra("doctor", doctor);
-            startActivity(intent2);
-        }
+    public void view_schedule(View view){
+        Intent intent = new Intent(getApplicationContext(), DoctorScheduleActivity.class);
+        intent.putExtra("doctor", passed_doctor);
+        startActivity(intent);
+    }
 }
-
